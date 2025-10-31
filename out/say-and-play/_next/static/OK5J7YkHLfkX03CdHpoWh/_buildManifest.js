f8565f3877b603cfe270d8f529b22f7ca3f1f43b
Danny Cabrera <danny@dannyfcabrera.com>
2025-10-31 11:40:15 -0400

    Add .nojekyll so GitHub Pages serves _next assets

---
+++ out/say-and-play/_next/static/OK5J7YkHLfkX03CdHpoWh/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/7c3d0ce75514c2d5.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/say-and-play//_next/:path+",
+        "destination": "/say-and-play/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
