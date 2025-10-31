2f8099dc86b5e6c799baa07c3618d6c8974734b1
Danny Cabrera <danny@dannyfcabrera.com>
2025-10-31 01:58:40 -0400

    Updates

--- _next/static/Y4M9M0XyK7TA1dzvVD3xE/_buildManifest.js
+++ _next/static/Y4M9M0XyK7TA1dzvVD3xE/_buildManifest.js
@@ -1,10 +1,15 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/2ceb68194f618eb7.js"
+    "./static/chunks/7c3d0ce75514c2d5.js"
   ],
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/say-and-play//_next/:path+",
+        "destination": "/say-and-play/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
--- _next/static/chunks/1c8216b427ab035c.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/ba592049100536e3.js",
-  "static/chunks/turbopack-1f8394cbe7d562a8.js"
-])
